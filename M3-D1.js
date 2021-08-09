58e81a0ab0e3c7c92a1495a7c4b20034573b1457
Emmanuel Iyere <40471024+emmanuelky@users.noreply.github.com>
2021-08-09 15:36:55 +0200

    finished ex2-ex11

--- M3-D1.js
+++ M3-D1.js
@@ -51,93 +51,141 @@ const findLargest = (num1, num2, num3) => {
     }
 
 }
-
-console.log(findLargest(0, 12, 30))
+console.log(findLargest(0, 12, 50))
 /*
 5)
 Create a function to check whether two numbers are in range 40..60 or in the range 70..100 inclusive.
 */
 
+const checkRange = (numb1, numb2) => {
+    if ((numb1 >= 40 && numb1 <= 60) || (numb1 >= 70 && numb1 <= 100)) {
+        return true
+    } else if ((numb2 >= 40 && numb2 <= 60) || (numb2 >= 70 && numb2 <= 100)) {
+        return true
+    }
+    return false
+}
+
+console.log(checkRange(15, 15))
 /*
 6)
 
 Create a function to create a new string of specified copies (positive number) of a given string.
 */
 
+const createNewStr = (str) => {
+    return str.substr(0, 7)
+}
+console.log(createNewStr("welcome"))
 /*
 
 7)
 Create a function to display the city name if the string begins with "Los" or "New" otherwise return blank.
 */
 
+const cityName = (str) => {
+    return (str.startsWith("Los") || str.startsWith("New")) ? str : " "
+}
+console.log(cityName("New york"))
 /*
 8)
 Create a function to calculate the sum of three elements of a given array of integers of length 3.
 */
 
+const sumElements = (arr) => {
+    let sum = 0
+    for (let num of arr) {
+        sum += num
+    }
+    return sum
+}
+
+console.log(sumElements([1, 2, 3]))
 /*
 
 9)
 Create a function to test whether an array of integers of length 2 contains 1 or a 3.
 */
 
+const testIncludeInt = (arr) => {
+    return (arr.includes(1) || arr.includes(3)) ? true : false
+}
+console.log(testIncludeInt([4, 6]))
 /*
 
 10)
 
 Create a function to test whether an array of integers of length 2 does not contain 1 or a 3
 
-11)
-
-Create a function to find the longest string from a given array of strings.
-
-12)
-
-Create a function to find the types of a given angle.
-
-Types of angles:
-    Acute angle: An angle between 0 and 90 degrees.
-    Right angle: An 90 degree angle.
-    btuse angle: An angle between 90 and 180 degrees.
-    Straight angle: A 180 degree angle.
-
-13)
-
-Create a function to find the index of the greatest element of a given array of integers
-
-14)
-
-Create a function to get the largest even number from an array of integers.
-
-16)
-
-Create a function to check from two given integers, whether one is positive and another one is negative.
-
-17)
-
-Create a function to create new string with first 3 characters are in lower case and the others in upper case. If the string length is less than 3 convert all the characters in upper case.
-
-18)
-
-Create a function to calculate the sum of the two given integers, If the sum is in the range 50..80 return 65 other wise return 80.
-
-19)
-
-Create a function to convert a number to a string, the contents of which depend on the number's factors. Follow next example:
-
-If the number has 3 as a factor, output 'Diego'.
-If the number has 5 as a factor, output 'Riccardo'.
-If the number has 7 as a factor, output 'Stefano'.
-If the number does not have 3, 5, or 7 as a factor, just pass the number's digits straight through.
-Examples
-28's factors are 1, 2, 4, 7, 14, 28.
-this would be a simple "Stefano".
-30's factors are 1, 2, 3, 5, 6, 10, 15, 30.
-this would be a "DiegoRiccardo".
-34 has four factors: 1, 2, 17, and 34.
-this would be "34".
-
-20)
-Create a function that given a phrase returns its acronym, like British Broadcasting Corporation returns BBC
-
 */
+
+const testNotIncludeInt = (arr) => {
+    return !(arr.includes(1) || arr.includes(3)) ? true : false
+}
+
+console.log(testNotIncludeInt([5, 8]))
+
+
+// 11)
+
+// Create a function to find the longest string from a given array of strings.
+
+const findLongeststr = (arr) => {
+    return arr.sort((a, b) => {
+        return b.length - a.length
+    })[0]
+
+}
+
+console.log(findLongeststr(["whatever", "mango", "pineapple"]))
+
+// 12)
+
+// Create a function to find the types of a given angle.
+
+// Types of angles:
+//     Acute angle: An angle between 0 and 90 degrees.
+//     Right angle: An 90 degree angle.
+//     btuse angle: An angle between 90 and 180 degrees.
+//     Straight angle: A 180 degree angle.
+
+// 13)
+
+// Create a function to find the index of the greatest element of a given array of integers
+
+// 14)
+
+// Create a function to get the largest even number from an array of integers.
+
+// 16)
+
+// Create a function to check from two given integers, whether one is positive and another one is negative.
+
+// 17)
+
+// Create a function to create new string with first 3 characters are in lower case and the others in upper case. If the string length is less than 3 convert all the characters in upper case.
+
+// 18)
+
+// Create a function to calculate the sum of the two given integers, If the sum is in the range 50..80 return 65 other wise return 80.
+
+// 19)
+
+// Create a function to convert a number to a string, the contents of which depend on the number's factors. Follow next example:
+
+// If the number has 3 as a factor, output 'Diego'.
+// If the number has 5 as a factor, output 'Riccardo'.
+// If the number has 7 as a factor, output 'Stefano'.
+// If the number does not have 3, 5, or 7 as a factor, just pass the number's digits straight through.
+// Examples
+// 28's factors are 1, 2, 4, 7, 14, 28.
+// this would be a simple "Stefano".
+// 30's factors are 1, 2, 3, 5, 6, 10, 15, 30.
+// this would be a "DiegoRiccardo".
+// 34 has four factors: 1, 2, 17, and 34.
+// this would be "34".
+
+// 20)
+// Create a function that given a phrase returns its acronym, like British Broadcasting Corporation returns BBC
+
+// */
